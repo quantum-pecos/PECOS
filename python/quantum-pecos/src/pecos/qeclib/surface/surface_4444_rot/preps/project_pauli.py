d0c734551bb1d5be7f8552ec14de3e981d18cde9
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2024-12-24 21:35:25 -0700

    Adding license notice

--- python/quantum-pecos/src/pecos/qeclib/surface/surface_4444_rot/preps/project_pauli.py
+++ python/quantum-pecos/src/pecos/qeclib/surface/surface_4444_rot/preps/project_pauli.py
@@ -9,6 +9,10 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+from pecos.slr import Block
 
-class Surface4444Rot:
-    """Rotated Surface code on 4.4.4.4 lattice."""
+# from pecos.qeclib.qubit
+
+
+class PrepProjectZ(Block):
+    """Prepare the +Z operator."""
