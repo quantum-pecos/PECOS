cfd1ff23971d70640a1fa5ac285dbbede2cc991f
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2024-12-24 22:01:41 -0700

    Switching to *qargs for PhysicalQubits

--- python/quantum-pecos/src/pecos/qeclib/qubit/qubit.py
+++ python/quantum-pecos/src/pecos/qeclib/qubit/qubit.py
@@ -1,41 +1,56 @@
 from pecos.slr import Qubit, Bit
-from pecos.qeclib.qubit import sq_paulis, sq_sqrt_paulis, sq_hadamards, tq_cliffords, measures
+from pecos.qeclib.qubit import sq_paulis, sq_sqrt_paulis, sq_hadamards, tq_cliffords, measures, preps
 
 # TODO accept multiple arguments like the underlying implementations
 
 class PhysicalQubit:
 
     @staticmethod
-    def x(qubit: Qubit):
+    def x(*qargs):
         """Pauli X gate"""
-        return sq_paulis.X(qubit)
+        return sq_paulis.X(*qargs)
 
     @staticmethod
-    def y(qubit: Qubit):
+    def y(*qargs):
         """Pauli Y gate"""
-        return sq_paulis.Y(qubit)
+        return sq_paulis.Y(*qargs)
 
     @staticmethod
-    def z(qubit: Qubit):
+    def z(*qargs):
         """Pauli Z gate"""
-        return sq_paulis.Z(qubit)
+        return sq_paulis.Z(*qargs)
 
     @staticmethod
-    def sz(qubit: Qubit):
+    def sz(*qargs):
         """Sqrt of Pauli Z gate"""
-        return sq_sqrt_paulis.SZ(qubit)
+        return sq_sqrt_paulis.SZ(*qargs)
 
     @staticmethod
-    def cx(ctrl: Qubit, trgt: Qubit):
-        """Controlled-X gate"""
-        return tq_cliffords.CX(ctrl, trgt)
-
-    @staticmethod
-    def h(qubit: Qubit):
+    def h(*qargs):
         """Hadamard gate"""
-        return sq_hadamards.H(qubit)
+        return sq_hadamards.H(*qargs)
 
     @staticmethod
-    def mz(qubit: Qubit, output: Bit):
+    def cx(*qargs):
+        """Controlled-X gate"""
+        return tq_cliffords.CX(*qargs)
+
+    @staticmethod
+    def cy(*qargs):
+        """Controlled-X gate"""
+        return tq_cliffords.CX(*qargs)
+
+    @staticmethod
+    def cz(*qargs):
+        """Controlled-X gate"""
+        return tq_cliffords.CX(*qargs)
+
+    @staticmethod
+    def pz(*qargs):
         """Measurement gate"""
-        return measures.Measure(qubit) > output
+        return preps.Prep(*qargs)
+
+    @staticmethod
+    def mz(qubits, outputs):
+        """Measurement gate"""
+        return measures.Measure(*qubits) > outputs
