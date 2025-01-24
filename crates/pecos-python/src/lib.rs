6b0a360c3d2ba9d5ff5075103eeed174766c7cd2
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2025-01-24 15:08:04 -0700

    fixing license header typo

--- crates/pecos-python/src/lib.rs
+++ crates/pecos-python/src/lib.rs
@@ -1,6 +1,6 @@
 // Copyright 2024 The PECOS Developers
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use thispub(crate)pub(crate) file except
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 // in compliance with the License.You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
